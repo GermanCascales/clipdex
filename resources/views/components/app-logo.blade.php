215d78218e29640fcd2756c1a62231d427d62872
exploractio <175453490+exploractio@users.noreply.github.com>
2025-05-10 21:56:28 +0000

    New size

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,3 +1,3 @@
 <div class="ml-1 grid flex-1 text-left text-sm">
-    <x-app-logo-icon class="size-24 fill-current text-white dark:text-black" />
+    <x-app-logo-icon class="w-20 h-10 fill-current text-white dark:text-black" aria-label="Clipdex" />
 </div>
