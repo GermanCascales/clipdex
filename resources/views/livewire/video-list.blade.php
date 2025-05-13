b2662b116de8e48d99f017b02a0febdc0ddc4a6b
exploractio <175453490+exploractio@users.noreply.github.com>
2025-05-13 18:44:59 +0000

    Cleanup of my-videos and video-list

--- resources/views/livewire/video-list.blade.php
+++ resources/views/livewire/video-list.blade.php
@@ -1,24 +1,19 @@
-<div>
-    <div class="">
-        <div class="max-w-7xl mx-auto">
-            <div class="overflow-hidden sm:rounded-lg">
+<div class="max-w-7xl mx-auto">
+    <div class="overflow-hidden sm:rounded-lg">
+        @if (session()->has('message'))
+        <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4">
+            {{ session('message') }}
+        </div>
+        @endif
 
-                @if (session()->has('message'))
-                    <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4">
-                        {{ session('message') }}
-                    </div>
-                @endif
+        <div class="columns-2 md:columns-3 lg:columns-4 gap-4 space-y-4">
+            @foreach($videos as $video)
+            <livewire:video-card :video="$video" :key="$video->id" />
+            @endforeach
+        </div>
 
-                <div class="columns-2 md:columns-3 lg:columns-4 gap-4 space-y-4">
-                    @foreach($videos as $video)
-                        <livewire:video-card :video="$video" :key="$video->id" />
-                    @endforeach
-                </div>
-
-                <div class="mt-6">
-                    {{ $videos->links() }}
-                </div>
-            </div>
+        <div class="mt-6">
+            {{ $videos->links() }}
         </div>
     </div>
 </div>
